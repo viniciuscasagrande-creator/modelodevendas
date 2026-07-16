0179f273c40d05e6fd66629bea93a0e4ebdccb02
Vinicius <vinicius@diskingressos.com.br>
2026-07-16 17:55:16 -0300

    build: allow all hosts in Vite config for local tunnel support

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
+  server: {
+    allowedHosts: true
+  }
 })
